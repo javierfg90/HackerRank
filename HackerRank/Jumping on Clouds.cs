4f07625f5d2047fcf2381c90a8a78c79778326de
Javier <javierfg90@nauta.cu>
2020-01-01 17:40:35 -0500

    My first commit

---
+++ HackerRank/Jumping on Clouds.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Jumping_on_Clouds
+    {
+        static int LCM(int a, int b)
+        {
+            int multiple = 0;
+            for (int i = 1; i <= b; i++)
+            {
+                if ((a * i) % b == 0)
+                {
+                    multiple = i;
+                    break;
+                }
+            }
+            return multiple;
+        }
+        static int jumpingOnClouds(int[] c, int k)
+        {
+            int[] visitedClouds = new int[LCM(k, c.Length)];
+            int i = 1;
+            visitedClouds[0] = c[0];
+            while ((i * k) % c.Length != 0)
+            {
+                visitedClouds[i] = c[(i * k) % c.Length];
+                i = i + 1;
+            }
+            int energyLevel = visitedClouds.Length;
+            for (int j = 0; j < visitedClouds.Length; j++)
+            {
+                if (visitedClouds[j] == 1)
+                {
+                    energyLevel += 2;
+                }
+            }
+            return 100 - energyLevel;
+        }
+    }
+}
---
+++ HackerRank/Pairs.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Pairs
+    {
+        private static int MinorPos(int[] a, int start)
+        {
+            int p = start;
+            for (int i = start + 1; i < a.Length; i++)
+                if (a[i] < a[p])
+                    p = i;
+            return p;
+
+        }
+        public static void Swap<T>(T[] a, int i, int j)
+        {
+            T temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+        public static void Order(int[] a)
+        {
+            for (int k = 0; k < a.Length; k++)
+                Swap(a, k, MinorPos(a, k));
+        }
+        static int pairs(int k, int[] arr)
+        {
+            Order(arr);
+            int i = 0;
+            int j = 1;
+            int count = 0;
+            while (i < arr.Length && j < arr.Length)
+            {
+                if ((i != j) && (arr[j] - arr[i] == k))
+                {
+                    count += 1;
+                    j++;
+                }
+                else if ((arr[j] - arr[i]) < k)
+                    j++;
+                else
+                    i++;
+            }
+            return count;
+        }
+    }
+}
