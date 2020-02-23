555ed368c7c5c8d29ffba2fc2e6391f17b0a02d6
Javier <javierfg90@nauta.cu>
2020-02-23 12:58:07 -0500

    Nice solution

---
+++ HackerRank/Delete Duplicate value nodes from a sorted Linked List.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    class Delete_Duplicate_value_nodes_from_a_sorted_Linked_List
+    {
+        //https://www.hackerrank.com/challenges/delete-duplicate-value-nodes-from-a-sorted-linked-list/problem
+        static SinglyLinkedListNode removeDuplicates(SinglyLinkedListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+            SinglyLinkedListNode aux = head.next;
+            while (aux != null && aux.data == head.data)
+            {
+                aux = aux.next;
+            }
+            head.next = removeDuplicates(aux);
+            return head;
+
+
+        }
+    }
+}
---
+++ HackerRank/Get the value of the node at a specific position from the tail.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    //https://www.hackerrank.com/challenges/get-the-value-of-the-node-at-a-specific-position-from-the-tail/problem
+    //Time Complexity=O(n)
+    class Get_the_value_of_the_node_at_a_specific_position_from_the_tail
+    {
+        static int getNode(SinglyLinkedListNode head, int positionFromTail)
+        {
+            int index = 0;
+            SinglyLinkedListNode temp = head;
+            SinglyLinkedListNode result = head;
+            while (temp != null)
+            {
+                temp = temp.next;
+                if (index > positionFromTail)
+                {
+                    result = result.next;
+                }
+                index++;
+            }
+            return result.data;
+
+
+
+
+        }
+    }
+}
